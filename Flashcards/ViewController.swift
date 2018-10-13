ccf9bf7098157cb0e0e80723bc572a3c9d45e17c
Ross <matchurozay@gmail.com>
2018-10-13 16:02:31 -0400

    Minimum optional update. Missing shadows and looks.

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -11,10 +11,24 @@ import UIKit
 class ViewController: UIViewController {
 
    
+    @IBOutlet weak var card: UIView!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var ansLabel: UILabel!
+    @IBOutlet weak var btnOptionOne: UIButton!
+    @IBOutlet weak var btnOptionTwo: UIButton!
+    @IBOutlet weak var btnOptionThree: UIButton!
+    var ans: String?
+    
     override func viewDidLoad() {
+        card.layer.cornerRadius = 20.0
+        card.layer.shadowRadius = 15.0
+        card.layer.shadowOpacity = 0.02
+        card.clipsToBounds = true
+        questionLabel.clipsToBounds = true
+        ansLabel.clipsToBounds = true
+        ans = ansLabel.text
         super.viewDidLoad()
+    
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -23,11 +37,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func didTapOnFlashcard(_ sender: Any) {
-        
-        questionLabel.isHidden = true;
+    @IBAction func didTapOptionOne(_ sender: Any) {
+      checkIfAns(btn: btnOptionOne)
+    }
+    
+    @IBAction func didTapOptionTwo(_ sender: Any) {
+      checkIfAns(btn: btnOptionTwo)
+    }
+    
+    @IBAction func didTapOptionThree(_ sender: Any) {
+       checkIfAns(btn: btnOptionThree)
+    }
+    
+    func checkIfAns(btn: UIButton){
+        if(btn.titleLabel?.text == ans )
+        {
+        questionLabel.isHidden = !questionLabel.isHidden;
+        }
+        else
+        {
+        btn.isHidden = true;
+        }
         
     }
+    
     
     
 
