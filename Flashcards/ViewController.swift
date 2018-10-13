ffafc0a019ddce3dbeef274ab9600dad07c69454
Ross <matchurozay@gmail.com>
2018-10-12 20:22:04 -0400

    Lab 1 Required actions Finished.

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var ansLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        
+        questionLabel.isHidden = true;
+        
+    }
+    
+    
 
 }
 
